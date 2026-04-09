4c0789137f110c0e48ebc9b4bcdd63c19ceda92b
camilamontes7e9-sys <montescami1103@gmail.com>
2026-04-09 23:10:08 +0200

    fix(gradle) FUNCIONAAAAA ;*;

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,23 @@
 plugins {
+
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21"
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
-    kotlin("plugin.serialization") version "1.9.0"
+    id("com.google.devtools.ksp") version "2.2.0-2.0.2"
+
 }
 
 android {
-    namespace = "com.example.mapsapiapp"
+    namespace = "com.example.mapsapp"
     compileSdk {
         version = release(36)
     }
 
     defaultConfig {
-        applicationId = "com.example.mapsapiapp"
-        minSdk = 24
+        applicationId = "com.example.mapsapp"
+        minSdk = 25
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -71,7 +74,6 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 
-
     implementation("com.google.maps.android:maps-compose:7.0.0")
     implementation("com.google.android.gms:play-services-maps:19.2.0")
     implementation("com.google.android.gms:play-services-location:21.3.0")
@@ -90,9 +92,50 @@ dependencies {
     implementation("io.ktor:ktor-client-android:2.3.8")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
 
+    // Navegació
+    implementation("androidx.navigation:navigation-compose:2.8.0")
+    // Serialització JSON
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+
+    //DEpendecies de maps
+    implementation("com.google.maps.android:maps-compose:7.0.0")
+    implementation("com.google.android.gms:play-services-maps:19.2.0")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+
+    implementation("com.google.maps.android:maps-ktx:4.0.0")
+
+    implementation("com.google.android.material:material:1.8.0")
+    implementation("androidx.compose.material:material-icons-extended")
+
+    // Supabase per a Bases de Dades (PostgREST)
+
+    // Ktor (Motor de xarxa per Android)
+    implementation("io.ktor:ktor-client-android:2.3.8")
+// Serialització JSON de Kotlin
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
+
+    //Room
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+
+
+    //Camera i galeria
+    implementation("io.coil-kt:coil-compose:2.7.0")
+
+    //Guardar fotos al supabase
+    implementation("io.github.jan-tennert.supabase:storage-kt:2.0.0")
+    implementation("io.ktor:ktor-client-android:latest_version")
+
+    implementation("io.github.jan-tennert.supabase:postgrest-kt:2.0.0")
+    implementation("io.github.jan-tennert.supabase:gotrue-kt:2.0.0")
+
 }
 
 secrets {
     propertiesFileName = "secrets.properties"
     defaultPropertiesFileName = "local.properties"
 }
+
+
+
+
--- app/src/main/java/com/example/mapsapiapp/ui/layout/MainScaffold.kt
+++ app/src/main/java/com/example/mapsapiapp/ui/layout/MainScaffold.kt
@@ -2,6 +2,8 @@ package com.example.mapsapiapp.ui.layout
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
